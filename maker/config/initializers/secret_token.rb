086773c546871c3521eba60ea4ae5d48fd164634
Ken Lu <ken@seeice.com>
2013-04-11 20:29:21 -0400

    added maker project

---
+++ maker/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maker::Application.config.secret_token = 'c36b583d07125b5f212231f9722509f4108717ec39fe92f1d38004db544547ed1a44690b8c85ddc358ca7698b29d98ce9cf3ff75fb1216bb6856a7bfba0925eb'
